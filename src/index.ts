99b3e287e65e17c86a5ec709f848fa358ea35102
zskio <vwind5224@gmail.com>
2022-08-12 15:16:50 +0400

    added express server and auto-migration

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import Database from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "blahreddit",
-    debug: !__prod__,
-    type: "postgresql",
-  });
+  const orm = await MikroORM.init(Database);
+  await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, {
-    title: "my first post ",
-    createdAt: new Date(),
-    updatedAt: new Date(),
+  const app = express();
+  app.get("/", (_, res) => {
+    res.send("hello");
   });
-  await orm.em.persistAndFlush(post);
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
-main();
+main().catch((err) => {
+  console.log(err);
+});

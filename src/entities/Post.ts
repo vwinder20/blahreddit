f430593c04e7e28aec8022da004bbf66b52a03be
zskio <vwind5224@gmail.com>
2022-08-12 15:16:10 +0400

    addded types for every property

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -3,14 +3,14 @@ import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 @Entity()
 export class Post {
   @PrimaryKey()
-  _id!: number;
+  id!: number;
 
-  @Property()
+  @Property({ type: "date" })
   createdAt: Date = new Date();
 
-  @Property({ onUpdate: () => new Date() })
+  @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
-  @Property()
+  @Property({ type: "text" })
   title!: string;
 }
